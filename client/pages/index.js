8d36816ae42763dfbe5bcce8b0fb01b1a9c22d82
Mayank Sharma <mayankece155@gmail.com>
2026-06-12 13:15:00 +0530

    Revert Changes

--- client/pages/index.js
+++ client/pages/index.js
@@ -87,9 +87,7 @@ const Home = ({ categories }) => {
     <Layout>
       <div className="row">
         <div className="col-md-12">
-          <h1 className="font-weight-bold">
-            Browse Tutorials/Courses---------
-          </h1>
+          <h1 className="font-weight-bold">Browse Tutorials/Courses</h1>
           <br />
         </div>
       </div>
